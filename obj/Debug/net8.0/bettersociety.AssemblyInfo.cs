17d921323405e4869317e332154f6ecca4be8a83
Harpal Singh <harryharpalsingh@outlook.com>
2025-01-15 22:41:56 +0530

    Save work before pulling changes

--- obj/Debug/net8.0/bettersociety.AssemblyInfo.cs
+++ obj/Debug/net8.0/bettersociety.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bettersociety")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37ab999ad1e0f4f604a3acc92165f635852710a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08614085dd0ac2ca79a4119e68afecfb98c45e12")]
 [assembly: System.Reflection.AssemblyProductAttribute("bettersociety")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bettersociety")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
